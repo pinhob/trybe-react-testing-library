993ace7990ad0dcf27c38c4d58cb5603ffe3f43d
Bruno Pinho <brunopinho@outlook.com>
2021-09-02 00:19:41 -0300

    feito requisito 2, teste 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import About from '../components/About';
+
+describe('2. Teste o componente <About.js />', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+    const headerElement = screen.getByRole('heading');
+    expect(headerElement).toBeInTheDocument();
+    expect(headerElement).toHaveTextContent('About Pokédex');
+  });
+});
