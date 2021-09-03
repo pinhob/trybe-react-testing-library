ac34f55f884e6f5f8113f6a37360d196ba5a336a
Bruno Pinho <brunopinho@outlook.com>
2021-09-03 11:33:22 -0300

    feito requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,10 +3,26 @@ import React from 'react';
 import About from '../components/About';
 
 describe('2. Teste o componente <About.js />', () => {
+  beforeEach(() => render(<About />));
+
   it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
-    render(<About />);
     const headerElement = screen.getByRole('heading');
     expect(headerElement).toBeInTheDocument();
     expect(headerElement).toHaveTextContent('About Pokédex');
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const firstParagraph = screen.getByText(/This application/i);
+    const secondParagraph = screen.getByText(/One can/i);
+
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a imagem de uma Pokédex', () => {
+    const pokedexImg = screen.getByRole('img');
+
+    expect(pokedexImg).toBeInTheDocument();
+    expect(pokedexImg).toHaveAttribute('src', expect.stringContaining('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'));
   });
 });
