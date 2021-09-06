7943ae0812667bd386bf09a743aa2aaf5e4b9256
Bruno Pinho <brunopinho@outlook.com>
2021-09-06 20:14:42 -0300

    criada função renderwithrouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('1. Teste o componente <App.js />', () => {
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+
+  });
+});
