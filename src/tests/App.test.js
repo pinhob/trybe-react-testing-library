2fa92afdde56f1d93562887257cfc5e76aa206c7
Bruno Pinho <brunopinho@outlook.com>
2021-09-06 20:15:50 -0300

    feito teste 1 do requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import React from 'react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 describe('1. Teste o componente <App.js />', () => {
   it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
 
+    const nav = screen.getByRole('navigation');
+    const firstLink = screen.getByRole('link', { name: /Home/i });
+    const secondLink = screen.getByRole('link', { name: /About/i });
+    const thirdLink = screen.getByRole('link', { name: /Favorite/i });
+
+    expect(nav).toBeInTheDocument();
+    expect(firstLink).toBeInTheDocument();
+    expect(secondLink).toBeInTheDocument();
+    expect(thirdLink).toBeInTheDocument();
   });
 });
