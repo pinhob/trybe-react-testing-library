ed891f04f2f4f69e4913324a526af985ff9cd11a
Bruno Pinho <brunopinho@outlook.com>
2021-09-07 14:13:06 -0300

    feito requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+const mockedPokemons = [
+  {
+    id: 65,
+    name: 'Alakazam',
+    type: 'Psychic',
+    averageWeight: {
+      value: '48.0',
+      measurementUnit: 'kg',
+    },
+  },
+  {
+    id: 143,
+    name: 'Snorlax',
+    type: 'Normal',
+    averageWeight: {
+      value: '460.0',
+      measurementUnit: 'kg',
+    },
+  },
+];
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('deve exibir na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noPokemonsMessage = screen.getByText(/No favorite pokemon found/i);
+    expect(noPokemonsMessage).toBeInTheDocument();
+  });
+
+  it('deve exibir todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ mockedPokemons } />);
+
+    const alakazamCard = screen.getByText(/Alakazam/i);
+    const snorlaxCard = screen.getByText(/Snorlax/i);
+
+    expect(alakazamCard).toBeInTheDocument();
+    expect(snorlaxCard).toBeInTheDocument();
+  });
+});
